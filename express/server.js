833debbf2df0ed8956e9476bde4b688c17574f95
abay-2002 <akbar121202@gmail.com>
2022-12-27 06:23:10 +0800

    modified

--- express/server.js
+++ express/server.js
@@ -363,7 +363,8 @@ router.post('/email-subscription', cors(), (req, res) => {
     });
 })
 
-router.post('/article-page-email-subscription', cors(), (req, res) => {
+router.post('/article', cors(), (req, res) => {
+    const contactUsURL = req.body.original_post_request_url;
     const options = {
         method: 'POST',
         url: 'https://api.notion.com/v1/pages',
@@ -489,8 +490,8 @@ router.post('/article-page-email-subscription', cors(), (req, res) => {
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
         console.log(`Success: ${req.body.user_email} just subscribed!`)
-        console.log(req.headers.host)
-        res.redirect(req.headers.host)
+        console.log(contactUsURL)
+        res.redirect(contactUsURL)
     });
 })
 
